fca4c3345eb11b592fdf9547808b498c4cffe314
Rachit <rachitchoudhary04@gmail.com>
2025-03-15 15:28:16 +0530

    modified:   DealSimplified/DealSimplified/settings.py

--- DealSimplified/DealSimplified/settings.py
+++ DealSimplified/DealSimplified/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+# Media files settings
+MEDIA_URL = '/media/'  # URL to access media files in development
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directory to store uploaded files

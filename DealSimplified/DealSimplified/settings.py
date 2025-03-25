ce0e7f906e15e95a7ad45fe37c5a13c567a323db
krishivg22 <kgeriani04@gmail.com>
2025-03-25 17:32:44 +0530

    connected to Railway postgres

--- DealSimplified/DealSimplified/settings.py
+++ DealSimplified/DealSimplified/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,14 +95,15 @@ WSGI_APPLICATION = 'DealSimplified.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dealsimplified',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',  # or your database server IP
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'dealsimplified',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',  # or your database server IP
+    #     'PORT': '5432',
+    # }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 
 
